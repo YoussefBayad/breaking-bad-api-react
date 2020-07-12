305d7d7797d5a2e66c8137f550bc9bb69fc873cd
Joseph bayad <jobayad66@gmail.com>
2020-07-12 17:22:21 +0100

    Updates

---
+++ precache-manifest.e6947cdce57a74c48dcf20a502013a64.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5af19e0556af4194165fefd30fb2a5d0",
+    "url": "/breaking-bad-api-react/index.html"
+  },
+  {
+    "revision": "622a394c337a1b62724a",
+    "url": "/breaking-bad-api-react/static/css/main.36e1a022.chunk.css"
+  },
+  {
+    "revision": "665cebd579e754094a31",
+    "url": "/breaking-bad-api-react/static/js/2.03c3de83.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/breaking-bad-api-react/static/js/2.03c3de83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "622a394c337a1b62724a",
+    "url": "/breaking-bad-api-react/static/js/main.0693732f.chunk.js"
+  },
+  {
+    "revision": "79d7f8b3f29c6dddad98",
+    "url": "/breaking-bad-api-react/static/js/runtime-main.8c6d7d28.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/breaking-bad-api-react/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/breaking-bad-api-react/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/breaking-bad-api-react/static/media/spinner.235b9b3e.gif"
+  }
+]);
